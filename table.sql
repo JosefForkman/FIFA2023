dd3818c0a9537903eee1fc2c12bdb8a97a546d18
Josef Forkman <strand.vatten@outlook.com>
2022-10-27 13:30:00 +0200

    add referee

---
+++ table.sql
@@ -0,0 +1,9 @@
+DROP TABLE "referee";
+CREATE TABLE referee (
+	id int NOT NULL PRIMARY KEY,
+	red_card int DEFAULT 0,
+	yellow_card int DEFAULT 0,
+	free_kicks int DEFAULT 0,
+	penalty_kicks int DEFAULT 0
+);
+
