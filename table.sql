0bd4c3e1bedfacd6c4d40a0536122fc08e21da25
Josef Forkman <strand.vatten@outlook.com>
2022-11-03 14:44:47 +0100

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,9 +1,37 @@
-DROP TABLE IF EXISTS "referee";
+DROP TABLE "coaches";
+CREATE TABLE coaches (
+	id int NOT NULL PRIMARY KEY,
+	redCard int DEFAULT 0,
+	yellowCard int DEFAULT 0	
+);
+
+DROP TABLE "referee";
 CREATE TABLE referee (
 	id int NOT NULL PRIMARY KEY,
-	red_card int DEFAULT 0,
-	yellow_card int DEFAULT 0,
-	free_kicks int DEFAULT 0,
-	penalty_kicks int DEFAULT 0
+	redCard int DEFAULT 0,
+	yellowCard int DEFAULT 0,
+	freeKicks int DEFAULT 0,
+	penaltyKicks int DEFAULT 0
+);
+
+DROP TABLE "team";
+CREATE TABLE team (
+	id int NOT NULL PRIMARY KEY,
+	name VARCHAR(30) NOT NULL UNIQUE,
+	abbreviation VARCHAR(6) NOT NULL UNIQUE,
+	flagURL VARCHAR(100) NOT NULL,
+	wins int DEFAULT 0,
+	draws int DEFAULT 0,	
+	losses int DEFAULT 0,	
+	scored int DEFAULT 0,
+	concededGoals int DEFAULT 0
+);
+
+DROP TABLE "venues";
+CREATE TABLE venues (
+	id int NOT NULL PRIMARY KEY,
+	city VARCHAR(30) NOT NULL UNIQUE,
+	name VARCHAR(30) NOT NULL,
+	capacity int NOT NULL DEFAULT 0
 );
 
